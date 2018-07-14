c423bc939f96dbd808f94fe4eee6f48e207279ff
austin <angium@126.com>
2018-07-14 21:35:59 +0800

    change to notify

--- libraries/AP_Notify/examples/AP_Notify_test/AP_Notify_test.cpp
+++ libraries/AP_Notify/examples/AP_Notify_test/AP_Notify_test.cpp
@@ -11,29 +11,30 @@ void setup();
 void loop();
 
 const AP_HAL::HAL& hal = AP_HAL::get_HAL();
+AP_Notify notify;
 
 // create board led object
-AP_BoardLED board_led;
+//AP_BoardLED board_led;
 
 void setup()
 {
     hal.console->printf("AP_Notify library test\n");
-
+	notify.init(true);
     // initialise the board leds
-    board_led.init();
+ //   board_led.init();
 
     // turn on initialising notification
-    AP_Notify::flags.initialising = true;
-    AP_Notify::flags.gps_status = 1;
-    AP_Notify::flags.armed = 1;
-    AP_Notify::flags.pre_arm_check = 1;
+ //   AP_Notify::flags.initialising = true;
+ //   AP_Notify::flags.gps_status = 1;
+ //   AP_Notify::flags.armed = 1;
+ //   AP_Notify::flags.pre_arm_check = 1;
 }
 
 void loop()
 {
 	hal.console->printf("initialising = %d",AP_Notify::flags.initialising);
-	board_led.update();
-	hal.scheduler->delay(100);
+	notify.update();
+	hal.scheduler->delay(20);
 }
 
 AP_HAL_MAIN();
