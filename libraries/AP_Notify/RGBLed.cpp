9dc0f4611ed045cfda7d2706bda48e652bece04c
austin <angium@126.com>
2018-07-15 14:38:45 +0800

    update_colour hz and green

--- libraries/AP_Notify/RGBLed.cpp
+++ libraries/AP_Notify/RGBLed.cpp
@@ -108,7 +108,7 @@ void RGBLed::update_colours(void)
     if (hal.gpio->usb_connected() && brightness > _led_dim) {
 		
 		hal.console->printf("usb_connected\n");
-        brightness = _led_dim;
+        brightness = _led_medium;
     }
 
     // initialising pattern
