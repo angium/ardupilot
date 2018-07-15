d094dcaab1ecf363a3199d248cda3d462f1fce67
austin <angium@126.com>
2018-07-15 08:37:03 +0800

    test only toshibaled_i2c external

--- libraries/AP_Notify/examples/AP_Notify_test/AP_Notify_test.cpp
+++ libraries/AP_Notify/examples/AP_Notify_test/AP_Notify_test.cpp
@@ -43,7 +43,7 @@ void loop()
 {
 	hal.console->printf("initialising = %d",AP_Notify::flags.initialising);
 	notify.update();
-	hal.scheduler->delay(5);
+	hal.scheduler->delay(50);
 }
 
 AP_HAL_MAIN();
