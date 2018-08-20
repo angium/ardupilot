48670e19c8f7ab0d85f0283ae9bfec175e4140fa
austin <angium@126.com>
2018-08-20 19:01:23 +0800

    defint fastloop 1

--- ArduSub/ArduSub.cpp
+++ ArduSub/ArduSub.cpp
@@ -16,7 +16,7 @@
 // ArduSub scheduling, originally copied from ArduCopter
 
 #include "Sub.h"
-#define USERHOOK_FASTLOOP
+#define USERHOOK_FASTLOOP 1
 
 
 #define SCHED_TASK(func, rate_hz, max_time_micros) SCHED_TASK_CLASS(Sub, &sub, func, rate_hz, max_time_micros)
