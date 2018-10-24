fcb748d1444b9f29b3af46fb7ad6d24b17d00c00
austin <angium@126.com>
2018-10-24 17:01:43 +0800

    print motor[i]

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -238,10 +238,9 @@ void AP_Motors6DOF::output_to_motors()
 	{
 		m_cnt = 0;
 		for (i=0; i<AP_MOTORS_MAX_NUM_MOTORS; i++) {
-        if (motor_enabled[i]) {
+
             hal.uartC->printf("motor_out[%d] = %d\n",i,motor_out[i]);
-        }
-    }
+	    }
 	}
     for (i=0; i<AP_MOTORS_MAX_NUM_MOTORS; i++) {
         if (motor_enabled[i]) {
