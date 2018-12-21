6a55d4b6add2413d9ed5edbef49f6e514ef1d0fb
austin <angium@126.com>
2018-12-21 16:36:46 +0800

    rpm

--- ArduSub/GCS_Mavlink.cpp
+++ ArduSub/GCS_Mavlink.cpp
@@ -649,6 +649,11 @@ bool GCS_MAVLINK_Sub::try_send_message(enum ap_message id)
         break;
 
     case MSG_RPM:
+		CHECK_PAYLOAD_SIZE(RPM);
+		hal.uartC->printf("MSG_RPM IS RUNNING\n");
+//		sub.send_rpm(MAVLINK_COMM_0);
+
+	
 #if RPM_ENABLED == ENABLED
         CHECK_PAYLOAD_SIZE(RPM);
 //		hal.uartC->printf("MSG_RPM IS RUNNING\n");
